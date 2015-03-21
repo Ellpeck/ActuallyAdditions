e274eb811f562e08a6713a0d371de4106c0dd4dd
Ellpeck <megamaximal@gmail.com>
2015-03-21 17:07:41 +0100

    Updated License.

--- src/main/java/ellpeck/gemification/items/ItemInfusedIron.java
+++
@@ -1,7 +0,0 @@
-package ellpeck.gemification.items;
-
-/**
- * Created by Ellpeck on 27.12.2014.
- */
-public class ItemInfusedIron{
-}
--- src/main/java/ellpeck/someprettyrandomstuff/inventory/SlotOredict.java
+++
@@ -1,7 +0,0 @@
-package ellpeck.someprettyrandomstuff.inventory;
-
-/**
- * Created by Ellpeck on 19.02.2015.
- */
-public class SlotOredict{
-}
