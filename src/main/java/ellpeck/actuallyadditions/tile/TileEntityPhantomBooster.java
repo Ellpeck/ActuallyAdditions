5077fed3412f82a21c2a906bd6c628947fe94b7b
Ellpeck <megamaximal@gmail.com>
2015-07-08 23:10:38 +0200

    Stopped the Phantom Booster from Updating

--- src/main/java/ellpeck/actuallyadditions/tile/TileEntityPhantomBooster.java
+++ src/main/java/ellpeck/actuallyadditions/tile/TileEntityPhantomBooster.java
@@ -2,4 +2,9 @@ package ellpeck.actuallyadditions.tile;
 
 public class TileEntityPhantomBooster extends TileEntityBase{
 
+    @Override
+    public boolean canUpdate(){
+        return false;
+    }
+
 }
