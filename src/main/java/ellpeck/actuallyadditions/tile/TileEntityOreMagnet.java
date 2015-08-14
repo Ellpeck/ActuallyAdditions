90f330fd97f44e44dc0662c2ea7a413ac8013e47
Ellpeck <megamaximal@gmail.com>
2015-08-14 19:25:01 +0200

    Added an extra Whitelist for the Ore Magnet. For stuff which isn't OreDictionaried. People know what I'm talking about.

--- src/main/java/ellpeck/actuallyadditions/tile/TileEntityOreMagnet.java
+++ src/main/java/ellpeck/actuallyadditions/tile/TileEntityOreMagnet.java
@@ -89,21 +89,18 @@ public class TileEntityOreMagnet extends TileEntityInventoryBase implements IEne
                                 Block block = worldObj.getBlock(xCoord+x, y, zCoord+z);
                                 int meta = worldObj.getBlockMetadata(xCoord+x, y, zCoord+z);
                                 if(block != null && !block.isAir(worldObj, xCoord+x, y, zCoord+z) && !block.hasTileEntity(meta) && block.getBlockHardness(worldObj, xCoord+x, y, zCoord+z) >= 0.0F && ((block.getMaterial() != null && block.getMaterial().isToolNotRequired()) || (block.getHarvestTool(meta) == null || (block.getHarvestTool(meta).equals("pickaxe") && block.getHarvestLevel(meta) <= 3)))){
+                                    //Check Whitelist
+                                    if(this.hasExtraWhitelist(block)){
+                                        this.removeBlock(x, y, z, block, meta, toPlaceY);
+                                        return;
+                                    }
+                                    //Check Ores
                                     int[] oreIDs = OreDictionary.getOreIDs(new ItemStack(block, 1, meta));
                                     for(int ID : oreIDs){
                                         String oreName = OreDictionary.getOreName(ID);
                                         //Is the block an ore according to the OreDictionary?
                                         if(oreName.substring(0, 3).equals("ore") && !this.hasException(oreName)){
-                                            //Remove the Block
-                                            worldObj.setBlockToAir(xCoord+x, y, zCoord+z);
-                                            worldObj.playAuxSFX(2001, xCoord+x, y, zCoord+z, Block.getIdFromBlock(block)+(meta << 12));
-
-                                            //Set the Block at the Top again
-                                            worldObj.setBlock(xCoord+x, yCoord+toPlaceY, zCoord+z, block, meta, 2);
-                                            worldObj.playSoundEffect((double)xCoord+x+0.5D, (double)yCoord+toPlaceY+0.5D, (double)zCoord+z+0.5D, block.stepSound.func_150496_b(), (block.stepSound.getVolume()+1.0F)/2.0F, block.stepSound.getPitch()*0.8F);
-
-                                            //Extract oil
-                                            this.tank.drain(oilUsePerTick, true);
+                                            this.removeBlock(x, y, z, block, meta, toPlaceY);
                                             return;
                                         }
                                     }
@@ -130,9 +127,32 @@ public class TileEntityOreMagnet extends TileEntityInventoryBase implements IEne
         }
     }
 
+    private void removeBlock(int x, int y, int z, Block block, int meta, int toPlaceY){
+        //Remove the Block
+        worldObj.setBlockToAir(xCoord+x, y, zCoord+z);
+        worldObj.playAuxSFX(2001, xCoord+x, y, zCoord+z, Block.getIdFromBlock(block)+(meta << 12));
+
+        //Set the Block at the Top again
+        worldObj.setBlock(xCoord+x, yCoord+toPlaceY, zCoord+z, block, meta, 2);
+        worldObj.playSoundEffect((double)xCoord+x+0.5D, (double)yCoord+toPlaceY+0.5D, (double)zCoord+z+0.5D, block.stepSound.func_150496_b(), (block.stepSound.getVolume()+1.0F)/2.0F, block.stepSound.getPitch()*0.8F);
+
+        //Extract oil
+        this.tank.drain(oilUsePerTick, true);
+    }
+
     private boolean hasException(String name){
         for(String except : ConfigValues.oreMagnetExceptions){
             if(except.equals(name)) return true;
+        }
+        return false;
+    }
+
+    private boolean hasExtraWhitelist(Block block){
+        String name = Block.blockRegistry.getNameForObject(block);
+        if(name != null){
+            for(String list : ConfigValues.oreMagnetExtraWhitelist){
+                if(list.equals(name)) return true;
+            }
         }
         return false;
     }
