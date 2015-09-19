583130025bd611bc5a9979d20309666433e4aeb9
Ellpeck <megamaximal@gmail.com>
2015-09-19 18:28:31 +0200

    0.0.9.0!

--- src/main/java/ellpeck/actuallyadditions/util/ModUtil.java
+++ src/main/java/ellpeck/actuallyadditions/util/ModUtil.java
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 
 public class ModUtil{
 
-    public static final String VERSION = "1.7.10-0.0.8.2";
+    public static final String VERSION = "1.7.10-0.0.9.0";
 
     public static final String MOD_ID = "ActuallyAdditions";
     public static final String NAME = "Actually Additions";
