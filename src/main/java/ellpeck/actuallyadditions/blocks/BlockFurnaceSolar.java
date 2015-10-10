23ae6402864d486ec476d31f5a244333d18f1d79
Ellpeck <megamaximal@gmail.com>
2015-10-10 15:29:02 +0200

    Solar Panel & Heat Collector energy storage info

--- src/main/java/ellpeck/actuallyadditions/blocks/BlockFurnaceSolar.java
+++ src/main/java/ellpeck/actuallyadditions/blocks/BlockFurnaceSolar.java
@@ -18,11 +18,13 @@ import ellpeck.actuallyadditions.util.IActAddItemOrBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
@@ -35,6 +37,18 @@ public class BlockFurnaceSolar extends BlockContainerBase implements IActAddItem
         this.setResistance(10.0F);
         this.setStepSound(soundTypeStone);
         this.setBlockBounds(0F, 0F, 0F, 1F, 3F/16F, 1F);
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9){
+        if(!world.isRemote){
+            TileEntityFurnaceSolar generator = (TileEntityFurnaceSolar)world.getTileEntity(x, y, z);
+            if(generator != null){
+                player.addChatComponentMessage(new ChatComponentText(generator.storage.getEnergyStored()+"/"+generator.storage.getMaxEnergyStored()+" RF"));
+            }
+            return true;
+        }
+        return true;
     }
 
     @Override
