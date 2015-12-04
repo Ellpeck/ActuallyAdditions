46ad9fee6fdf85937152193296089d9b7de66a8b
Ellpeck <megamaximal@gmail.com>
2015-12-04 22:42:21 +0100

    right..

--- src/main/java/ellpeck/actuallyadditions/booklet/GuiBooklet.java
+++ src/main/java/ellpeck/actuallyadditions/booklet/GuiBooklet.java
@@ -168,7 +168,7 @@ public class GuiBooklet extends GuiScreen{
 
     @Override
     public void keyTyped(char theChar, int key){
-        if(key == Keyboard.KEY_ESCAPE || key == Minecraft.getMinecraft().gameSettings.keyBindInventory.getKeyCode()){
+        if(key == Keyboard.KEY_ESCAPE || (!this.searchField.isFocused() && key == Minecraft.getMinecraft().gameSettings.keyBindInventory.getKeyCode())){
             if(this.parentScreen != null){
                 this.mc.displayGuiScreen(this.parentScreen);
             }
