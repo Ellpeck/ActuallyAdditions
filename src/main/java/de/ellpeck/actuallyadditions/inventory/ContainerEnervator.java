caf3fac20836a549966fd0b4e3aa969151de93a7
Ellpeck <megamaximal@gmail.com>
2016-01-03 16:05:51 +0100

    Update Copyright

--- src/main/java/de/ellpeck/actuallyadditions/inventory/ContainerEnervator.java
+++ src/main/java/de/ellpeck/actuallyadditions/inventory/ContainerEnervator.java
@@ -2,10 +2,10 @@
  * This file ("ContainerEnervator.java") is part of the Actually Additions Mod for Minecraft.
  * It is created and owned by Ellpeck and distributed
  * under the Actually Additions License to be found at
- * http://github.com/Ellpeck/ActuallyAdditions/blob/master/README.md
+ * http://ellpeck.de/actaddlicense/
  * View the source code at https://github.com/Ellpeck/ActuallyAdditions
  *
- * © 2015 Ellpeck
+ * © 2016 Ellpeck
  */
 
 package de.ellpeck.actuallyadditions.inventory;
--- src/main/java/de/ellpeck/actuallyadditions/util/CompatUtil.java
+++ src/main/java/de/ellpeck/actuallyadditions/util/CompatUtil.java
@@ -2,7 +2,7 @@
  * This file ("CompatUtil.java") is part of the Actually Additions Mod for Minecraft.
  * It is created and owned by Ellpeck and distributed
  * under the Actually Additions License to be found at
- * http://github.com/Ellpeck/ActuallyAdditions/blob/master/README.md
+ * http://ellpeck.de/actaddlicense/
  * View the source code at https://github.com/Ellpeck/ActuallyAdditions
  *
  * © 2016 Ellpeck
