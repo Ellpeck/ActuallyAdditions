4619034e33789fdcabdc32c92009088f76a802bc
Ellpeck <megamaximal@gmail.com>
2016-08-06 17:08:40 +0200

    I thought this was obvious >_>

--- src/main/java/de/ellpeck/actuallyadditions/mod/inventory/gui/GuiBag.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/inventory/gui/GuiBag.java
@@ -107,6 +107,7 @@ public class GuiBag extends GuiContainer{
             List<String> text = new ArrayList<String>();
             text.add(TextFormatting.BOLD+"Auto-Insert "+(this.container.autoInsert ? "On" : "Off"));
             text.addAll(this.mc.fontRendererObj.listFormattedStringToWidth("Turn this on to make items that get picked up automatically go into the bag.", 200));
+            text.addAll(this.mc.fontRendererObj.listFormattedStringToWidth(TextFormatting.GRAY+""+TextFormatting.ITALIC+"Note that this WON'T work when you are holding the bag in your hand.", 200));
             this.drawHoveringText(text, mouseX, mouseY);
         }
     }
