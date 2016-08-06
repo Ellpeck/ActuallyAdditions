168d21010bd9d4609b2a61d2c203224416e0d769
Ellpeck <megamaximal@gmail.com>
2016-08-07 00:02:26 +0200

    Adjust the special renderer a bit and make it disappear when Elytra flying because it looks stupid

--- src/main/java/de/ellpeck/actuallyadditions/mod/misc/special/RenderSpecial.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/misc/special/RenderSpecial.java
@@ -10,19 +10,14 @@
 
 package de.ellpeck.actuallyadditions.mod.misc.special;
 
-import de.ellpeck.actuallyadditions.mod.proxy.ClientProxy;
 import de.ellpeck.actuallyadditions.mod.util.AssetUtil;
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EnumPlayerModelParts;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Vec3d;
-
-import java.util.Calendar;
 
 public class RenderSpecial{
 
@@ -33,45 +28,38 @@ public class RenderSpecial{
     }
 
     public void render(EntityPlayer player, float partialTicks){
-        if(player.isInvisible() || !player.isWearing(EnumPlayerModelParts.CAPE)){
+        if(player.isInvisible() || !player.isWearing(EnumPlayerModelParts.CAPE) || player.isElytraFlying()){
             return;
         }
-
         boolean isBlock = this.theThingToRender.getItem() instanceof ItemBlock;
-        float size = isBlock ? 0.5F : 0.4F;
-        double offsetUp = isBlock ? 0D : 0.1875D;
-
-        double bobHeight = 0.3;
-        double boop = Minecraft.getSystemTime()/1000D;
 
         GlStateManager.pushMatrix();
 
         Vec3d currentPos = Minecraft.getMinecraft().thePlayer.getPositionEyes(partialTicks);
         Vec3d playerPos = player.getPositionEyes(partialTicks);
-        GlStateManager.translate(playerPos.xCoord-currentPos.xCoord, playerPos.yCoord-currentPos.yCoord-(player.isSneaking() || Minecraft.getMinecraft().thePlayer.isSneaking() ? 0.125D : 0D), playerPos.zCoord-currentPos.zCoord);
+        GlStateManager.translate(playerPos.xCoord-currentPos.xCoord, playerPos.yCoord-currentPos.yCoord, playerPos.zCoord-currentPos.zCoord);
 
-        GlStateManager.translate(0D, 2.435D+offsetUp, 0D);
+        GlStateManager.translate(0D, 2.375D-(player.isSneaking() ? 0.125D : 0D)+(isBlock ? 0D : 0.1875D), 0D);
         GlStateManager.rotate(180F, 1.0F, 0.0F, 1.0F);
+
+        float size = isBlock ? 0.5F : 0.4F;
         GlStateManager.scale(size, size, size);
 
         //Make the floaty stuff look nice using sine waves \o/ -xdjackiexd
         //Peck edit: What do you mean by "nice" you jackass? >_>
-        GlStateManager.translate(0D, Math.sin(boop%(2*Math.PI))*bobHeight, 0D);
+        double boop = Minecraft.getSystemTime()/1000D;
+        GlStateManager.translate(0D, Math.sin(boop%(2*Math.PI))*0.25, 0D);
         GlStateManager.rotate((float)(((boop*40D)%360)), 0, 1, 0);
 
         GlStateManager.disableLighting();
         if(this.theThingToRender != null){
-            if(isBlock){
-                GlStateManager.rotate(180F, 1F, 0F, 0F);
-                AssetUtil.renderBlockInWorld(Block.getBlockFromItem(this.theThingToRender.getItem()), this.theThingToRender.getItemDamage());
-            }
-            else{
-                GlStateManager.pushMatrix();
+            GlStateManager.pushMatrix();
+            if(!isBlock){
                 GlStateManager.translate(0D, 0.5D, 0D);
-                GlStateManager.rotate(180F, 1F, 0F, 0F);
-                AssetUtil.renderItemInWorld(this.theThingToRender);
-                GlStateManager.popMatrix();
             }
+            GlStateManager.rotate(180F, 1F, 0F, 0F);
+            AssetUtil.renderItemInWorld(this.theThingToRender);
+            GlStateManager.popMatrix();
         }
         GlStateManager.enableLighting();
 
