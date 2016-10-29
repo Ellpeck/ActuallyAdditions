8f53736f6ddffd8da0c6b9e88b04bd3588c558e6
Ellpeck <megamaximal@gmail.com>
2016-10-29 12:29:13 +0200

    Made empowering possible with any dyes, not just vanilla ones Closes #298

--- src/main/java/de/ellpeck/actuallyadditions/mod/recipe/EmpowererHandler.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/recipe/EmpowererHandler.java
@@ -32,17 +32,16 @@ public final class EmpowererHandler{
     public static EmpowererRecipe recipeEmpoweredCanolaSeed;
 
     public static void init(){
-        addCrystalEmpowering(TheCrystals.REDSTONE, new ItemStack(Items.DYE, 1, EnumDyeColor.RED.getDyeDamage()), new ItemStack(Items.NETHERBRICK), new ItemStack(Items.REDSTONE), new ItemStack(Items.BRICK));
-        addCrystalEmpowering(TheCrystals.LAPIS, new ItemStack(Items.DYE, 1, EnumDyeColor.CYAN.getDyeDamage()), new ItemStack(Items.PRISMARINE_SHARD), new ItemStack(Items.PRISMARINE_SHARD), new ItemStack(Items.PRISMARINE_SHARD));
-        addCrystalEmpowering(TheCrystals.DIAMOND, new ItemStack(Items.DYE, 1, EnumDyeColor.LIGHT_BLUE.getDyeDamage()), new ItemStack(Items.CLAY_BALL), new ItemStack(Items.CLAY_BALL), new ItemStack(Blocks.CLAY));
-        addCrystalEmpowering(TheCrystals.IRON, new ItemStack(Items.DYE, 1, EnumDyeColor.GRAY.getDyeDamage()), new ItemStack(Items.SNOWBALL), new ItemStack(Blocks.STONE_BUTTON), new ItemStack(Blocks.COBBLESTONE));
+        addCrystalEmpowering(TheCrystals.REDSTONE, "dyeRed", new ItemStack(Items.NETHERBRICK), new ItemStack(Items.REDSTONE), new ItemStack(Items.BRICK));
+        addCrystalEmpowering(TheCrystals.LAPIS, "dyeCyan", new ItemStack(Items.PRISMARINE_SHARD), new ItemStack(Items.PRISMARINE_SHARD), new ItemStack(Items.PRISMARINE_SHARD));
+        addCrystalEmpowering(TheCrystals.DIAMOND, "dyeLightBlue", new ItemStack(Items.CLAY_BALL), new ItemStack(Items.CLAY_BALL), new ItemStack(Blocks.CLAY));
+        addCrystalEmpowering(TheCrystals.IRON, "dyeGray", new ItemStack(Items.SNOWBALL), new ItemStack(Blocks.STONE_BUTTON), new ItemStack(Blocks.COBBLESTONE));
 
-        addCrystalEmpowering(TheCrystals.COAL, new ItemStack(Items.DYE, 1, EnumDyeColor.BLACK.getDyeDamage()), new ItemStack(Items.COAL, 1, 1), new ItemStack(Items.FLINT), new ItemStack(Blocks.STONE));
-        addCrystalEmpowering(TheCrystals.COAL, new ItemStack(InitItems.itemMisc, 1, TheMiscItems.BLACK_DYE.ordinal()), new ItemStack(Items.COAL, 1, 1), new ItemStack(Items.FLINT), new ItemStack(Blocks.STONE));
+        addCrystalEmpowering(TheCrystals.COAL, "dyeBlack", new ItemStack(Items.COAL, 1, 1), new ItemStack(Items.FLINT), new ItemStack(Blocks.STONE));
 
         List<ItemStack> balls = OreDictionary.getOres("slimeball");
         for(ItemStack ball : balls){
-            addCrystalEmpowering(TheCrystals.EMERALD, new ItemStack(Items.DYE, 1, EnumDyeColor.LIME.getDyeDamage()), new ItemStack(Blocks.TALLGRASS, 1, 1), new ItemStack(Blocks.SAPLING), ball.copy());
+            addCrystalEmpowering(TheCrystals.EMERALD, "dyeLime", new ItemStack(Blocks.TALLGRASS, 1, 1), new ItemStack(Blocks.SAPLING), ball.copy());
         }
 
         ItemStack seed = new ItemStack(InitItems.itemCanolaSeed);
@@ -50,11 +49,15 @@ public final class EmpowererHandler{
         recipeEmpoweredCanolaSeed = RecipeUtil.lastEmpowererRecipe();
     }
 
-    private static void addCrystalEmpowering(TheCrystals type, ItemStack modifier1, ItemStack modifier2, ItemStack modifier3, ItemStack modifier4){
+    private static void addCrystalEmpowering(TheCrystals type, String dye, ItemStack modifier1, ItemStack modifier2, ItemStack modifier3){
         float[] color = type.conversionColorParticles;
-        ActuallyAdditionsAPI.addEmpowererRecipe(new ItemStack(InitItems.itemCrystal, 1, type.ordinal()), new ItemStack(InitItems.itemCrystalEmpowered, 1, type.ordinal()), modifier1, modifier2, modifier3, modifier4, 5000, 50, color);
-        MAIN_PAGE_RECIPES.add(RecipeUtil.lastEmpowererRecipe());
-        ActuallyAdditionsAPI.addEmpowererRecipe(new ItemStack(InitBlocks.blockCrystal, 1, type.ordinal()), new ItemStack(InitBlocks.blockCrystalEmpowered, 1, type.ordinal()), modifier1, modifier2, modifier3, modifier4, 50000, 500, color);
-        MAIN_PAGE_RECIPES.add(RecipeUtil.lastEmpowererRecipe());
+
+        List<ItemStack> dyes = OreDictionary.getOres(dye);
+        for(ItemStack dyeStack : dyes){
+            ActuallyAdditionsAPI.addEmpowererRecipe(new ItemStack(InitItems.itemCrystal, 1, type.ordinal()), new ItemStack(InitItems.itemCrystalEmpowered, 1, type.ordinal()), dyeStack, modifier1, modifier2, modifier3, 5000, 50, color);
+            MAIN_PAGE_RECIPES.add(RecipeUtil.lastEmpowererRecipe());
+            ActuallyAdditionsAPI.addEmpowererRecipe(new ItemStack(InitBlocks.blockCrystal, 1, type.ordinal()), new ItemStack(InitBlocks.blockCrystalEmpowered, 1, type.ordinal()), dyeStack, modifier1, modifier2, modifier3, 50000, 500, color);
+            MAIN_PAGE_RECIPES.add(RecipeUtil.lastEmpowererRecipe());
+        }
     }
 }
