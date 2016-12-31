c5c5b2a08c57cc6fe693e304dcb6871f2a0b7c61
Ellpeck <megamaximal@gmail.com>
2016-12-31 18:10:36 +0100

    withered edition (apparently)

--- src/main/java/de/ellpeck/actuallyadditions/mod/booklet/gui/GuiMainPage.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/booklet/gui/GuiMainPage.java
@@ -140,6 +140,9 @@ public class GuiMainPage extends GuiBooklet{
         else if(playerName.equalsIgnoreCase("xbony2")){
             this.bookletEdition = "Naughty Edition";
         }
+        else if(playerName.equalsIgnoreCase("themattabase")){
+            this.bookletEdition = "Withered Edition";
+        }
         else{
             if(Util.isDevVersion()){
                 this.bookletEdition = "Dev's Edition";
