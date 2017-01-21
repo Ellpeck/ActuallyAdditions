3c07f4535d7a754231d28ab6fb1b2b57be9c92ab
Ellpeck <megamaximal@gmail.com>
2017-01-21 13:12:28 +0100

    Stop the storage crate from syncing its NBT Closes #586

--- src/main/java/de/ellpeck/actuallyadditions/mod/blocks/BlockGiantChest.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/blocks/BlockGiantChest.java
@@ -186,5 +186,10 @@ public class BlockGiantChest extends BlockContainerBase{
                 tooltip.add(TextFormatting.ITALIC+"'Small'");
             }
         }
+
+        @Override
+        public NBTTagCompound getNBTShareTag(ItemStack stack){
+            return null;
+        }
     }
 }
