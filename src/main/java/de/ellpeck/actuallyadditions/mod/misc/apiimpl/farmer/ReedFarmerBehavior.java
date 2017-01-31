75d2de8438069599117af9b28d708e8ec1e83628
Daniel Mansfield <daniel.mansfield99@gmail.com>
2017-01-31 17:49:42 +0000

    Added sugar cane support to the Farmer (#611)

---
+++ src/main/java/de/ellpeck/actuallyadditions/mod/misc/apiimpl/farmer/ReedFarmerBehavior.java
@@ -0,0 +1,75 @@
+/*
+ * This file ("CrusherRecipeHandler.java") is part of the Actually Additions mod for Minecraft.
+ * It is created and owned by Ellpeck and distributed
+ * under the Actually Additions License to be found at
+ * http://ellpeck.de/actaddlicense
+ * View the source code at https://github.com/Ellpeck/ActuallyAdditions
+ *
+ * © 2015-2017 Ellpeck
+ */
+
+package de.ellpeck.actuallyadditions.mod.misc.apiimpl.farmer;
+
+import de.ellpeck.actuallyadditions.api.farmer.IFarmerBehavior;
+import de.ellpeck.actuallyadditions.api.internal.IFarmer;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockReed;
+import net.minecraft.block.BlockSand;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class ReedFarmerBehavior implements IFarmerBehavior{
+
+    @Override
+    public boolean tryPlantSeed(ItemStack seed, World world, BlockPos pos, IFarmer farmer){
+        int use = 250;
+        if(farmer.getEnergy() >= use){
+            if(seed.getItem() == Items.REEDS){
+                if(Blocks.REEDS.canPlaceBlockAt(world, pos)){
+                    world.setBlockState(pos, Blocks.REEDS.getDefaultState(), 2);
+                    farmer.extractEnergy(use);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean tryHarvestPlant(World world, BlockPos pos, IFarmer farmer){
+        int use = 250;
+        if(farmer.getEnergy() >= use){
+            IBlockState state = world.getBlockState(pos);
+            if(state.getBlock() instanceof BlockReed){
+                for(int i = 2; i >= 1; --i){
+                    if(farmer.getEnergy() >= use){
+                        BlockPos up = pos.up(i);
+                        IBlockState upState = world.getBlockState(up);
+                        if(upState.getBlock() instanceof BlockReed){
+                            List<ItemStack> drops = upState.getBlock().getDrops(world, up, upState, 0);
+
+                            if(drops != null && !drops.isEmpty()){
+                                if(farmer.addToOutputInventory(drops, false)){
+                                    world.playEvent(2011, up, Block.getStateId(upState));
+                                    world.setBlockToAir(up);
+
+                                    farmer.extractEnergy(use);
+                                    farmer.addToOutputInventory(drops, true);
+                                }
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
