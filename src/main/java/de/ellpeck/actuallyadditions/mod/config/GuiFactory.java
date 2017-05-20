62d074e758841a3802c794730ea6e4e3318c6d37
Ellpeck <megamaximal@gmail.com>
2017-05-20 21:05:25 +0200

    updated forge

--- src/main/java/de/ellpeck/actuallyadditions/mod/config/GuiFactory.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/config/GuiFactory.java
@@ -24,6 +24,16 @@ public class GuiFactory implements IModGuiFactory{
     }
 
     @Override
+    public boolean hasConfigGui(){
+        return true;
+    }
+
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen){
+        return new GuiConfiguration(parentScreen);
+    }
+
+    @Override
     public Class<? extends GuiScreen> mainConfigGuiClass(){
         return GuiConfiguration.class;
     }
