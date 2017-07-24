817579e242c4ec37a6399b37815d4a41c43af9c0
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-23 23:23:34 -0400

    Resolve dupe glitch
    Items using custom item entities return false in EntityJoinWorldEvent,
    making World#spawnEntity return false, making the stackSize not drop
    properly.

--- src/main/java/de/ellpeck/actuallyadditions/mod/tile/TileEntityDropper.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/tile/TileEntityDropper.java
@@ -67,9 +67,8 @@ public class TileEntityDropper extends TileEntityInventoryBase{
         ItemStack theoreticalRemove = this.removeFromInventory(false);
         if(StackUtil.isValid(theoreticalRemove)){
             IBlockState state = this.world.getBlockState(this.pos);
-            if(WorldUtil.dropItemAtSide(WorldUtil.getDirectionByPistonRotation(state.getBlock().getMetaFromState(state)), this.world, this.pos, StackUtil.setStackSize(theoreticalRemove.copy(), 1))){
+            WorldUtil.dropItemAtSide(WorldUtil.getDirectionByPistonRotation(state.getBlock().getMetaFromState(state)), this.world, this.pos, StackUtil.setStackSize(theoreticalRemove.copy(), 1));
                 this.removeFromInventory(true);
-            }
         }
     }
 
