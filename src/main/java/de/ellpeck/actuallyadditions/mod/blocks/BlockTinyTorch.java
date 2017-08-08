2ee695359cabda37b3013efcba23cd8a42b4aaa4
Shadows_of_Fire <Bward7864@gmail.com>
2017-08-08 08:01:28 -0400

    Fixes #880
    probably, anyway

--- src/main/java/de/ellpeck/actuallyadditions/mod/blocks/BlockTinyTorch.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/blocks/BlockTinyTorch.java
@@ -15,6 +15,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockTorch;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -81,6 +82,15 @@ public class BlockTinyTorch extends BlockBase{
     @Override
     public boolean isFullCube(IBlockState state){
         return false;
+    }
+    
+    @Override
+    public boolean isNormalCube(IBlockState state){
+        return false;
+    }
+    
+    public BlockFaceShape getBlockFaceShape(IBlockAccess world, IBlockState state, BlockPos pos, EnumFacing facing) {
+        return BlockFaceShape.UNDEFINED;
     }
 
     private boolean canPlaceOn(World worldIn, BlockPos pos){
