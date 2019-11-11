6ef5e409c7b8a29e6d770e3472cd27dde2859562
Brennan Ward <Bward7864@gmail.com>
2019-11-11 18:52:54 -0500

    Convert to skeletal project

---
+++ src/main/java/de/ellpeck/actuallyadditions/mod/item/AAItems.java
@@ -0,0 +1,20 @@
+package de.ellpeck.actuallyadditions.mod.item;
+
+import de.ellpeck.actuallyadditions.mod.ActuallyAdditions;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.ObjectHolder;
+
+@EventBusSubscriber(modid = ActuallyAdditions.MODID, bus = Bus.MOD)
+@ObjectHolder(ActuallyAdditions.MODID)
+public class AAItems {
+
+	@SubscribeEvent
+	public static void register(Register<Item> e) {
+
+	}
+
+}
