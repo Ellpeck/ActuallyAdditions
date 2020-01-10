7d948942c3e2ae25948deb272faab637da9ba824
Vapor-X\Fanamy <patricia.berger@wavenet.at>
2020-01-10 15:42:56 +0100

    feat(lamp) added lamp recipes

--- src/main/java/de/ellpeck/actuallyadditions/mod/item/AAItems.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/item/AAItems.java
@@ -14,16 +14,14 @@ import net.minecraftforge.registries.ObjectHolder;
 public class AAItems {
 
   public static final Item BLACK_QUARTZ = null;
-  public static final Item PALIS_CRYSTAL = null;
 
   @SubscribeEvent
   public static void register(Register<Item> e) {
     //Formatter::off
     e.getRegistry().registerAll(
         new Item(new Properties().group(ActuallyAdditions.GROUP)).setRegistryName("black_quartz"),
-        new Item(new Properties().group(ActuallyAdditions.GROUP)).setRegistryName("palis_crystal")
+        new Item(new Properties().group(ActuallyAdditions.GROUP)).setRegistryName("crystal_blue")
     );
     //Formatter::on
   }
-
 }
