e112c040cd23308d6132deed22fe3169c7df934e
Eike Lorbach <eike.lorb@gmail.com>
2020-01-10 15:13:03 +0100

    refactor (AABlocks): summarized LampColor in register

--- src/main/java/de/ellpeck/actuallyadditions/mod/block/ColoredLampBlock.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/block/ColoredLampBlock.java
@@ -14,47 +14,50 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
+
 public class ColoredLampBlock extends RedstoneLampBlock {
 
-	public static final EnumMap<LampColor, ColoredLampBlock> LAMPS = new EnumMap<>(LampColor.class);
+    public static final EnumMap<LampColor, ColoredLampBlock> LAMPS = new EnumMap<>(LampColor.class);
 
-	protected LampColor color;
+    protected LampColor color;
 
-	public ColoredLampBlock(LampColor color) {
-		super(Properties.create(Material.REDSTONE_LIGHT).harvestTool(ToolType.PICKAXE).lightValue(15).hardnessAndResistance(0.5F, 3.0F));
-		this.setDefaultState(getDefaultState().with(LIT, false));
-		this.color = color;
-		LAMPS.put(color, this);
-	}
+    public ColoredLampBlock(LampColor color) {
+        super(Properties.create(Material.REDSTONE_LIGHT).harvestTool(ToolType.PICKAXE).lightValue(15).hardnessAndResistance(0.5F, 3.0F));
+        this.setDefaultState(getDefaultState().with(LIT, false));
+        this.color = color;
+        LAMPS.put(color, this);
+    }
 
-	@Override
-	@Deprecated
-	public boolean onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
-		ItemStack stack = player.getHeldItem(hand);
-		if (player.isSneaking() && stack.isEmpty()) {
-			world.setBlockState(pos, state.cycle(LIT));
-			return true;
-		}
 
-		LampColor color = LampColor.getColorFromStack(stack);
-		if (color != null && color != this.color) {
-			world.setBlockState(pos, LAMPS.get(color).getDefaultState().with(LIT, state.get(LIT)));
-			if (!player.abilities.isCreativeMode) {
-				stack.shrink(1);
-			}
-			return true;
-		}
+    @Override
+    @Deprecated
+    public boolean onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
+        ItemStack stack = player.getHeldItem(hand);
+        if (player.isSneaking() && stack.isEmpty()) {
+            world.setBlockState(pos, state.cycle(LIT));
+            return true;
+        }
 
-		return false;
-	}
+        LampColor color = LampColor.getColorFromStack(stack);
+        if (color != null && color != this.color) {
+            world.setBlockState(pos, LAMPS.get(color).getDefaultState().with(LIT, state.get(LIT)));
+            if (! player.abilities.isCreativeMode) {
+                stack.shrink(1);
+            }
+            return true;
+        }
 
-	@Override
-	public void neighborChanged(BlockState state, World world, BlockPos pos, Block from, BlockPos fromPos, boolean isMoving) {
-		if (!world.isRemote) {
-			if (world.getBlockState(fromPos).canProvidePower()) {
-				world.setBlockState(pos, state.with(LIT, world.isBlockPowered(pos)));
-			}
-		}
-	}
+        return false;
+    }
+
+
+    @Override
+    public void neighborChanged(BlockState state, World world, BlockPos pos, Block from, BlockPos fromPos, boolean isMoving) {
+        if (! world.isRemote) {
+            if (world.getBlockState(fromPos).canProvidePower()) {
+                world.setBlockState(pos, state.with(LIT, world.isBlockPowered(pos)));
+            }
+        }
+    }
 
 }
