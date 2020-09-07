7e2be8712e37504ae429f09c1b864f61f2ad4d95
canitzp <canitzp@gmail.com>
2020-09-07 07:37:56 +0200

    Added tiny coal/charcoal and biocoal

--- src/main/java/de/ellpeck/actuallyadditions/mod/item/AAItems.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/item/AAItems.java
@@ -3,6 +3,7 @@ package de.ellpeck.actuallyadditions.mod.item;
 import de.ellpeck.actuallyadditions.mod.ActuallyAdditions;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -14,12 +15,18 @@ import net.minecraftforge.registries.ObjectHolder;
 public class AAItems {
 
 	public static final Item BLACK_QUARTZ = null;
+	public static final BurnableItem TINY_COAL = null;
+	public static final BurnableItem TINY_CHARCOAL = null;
+	public static final BurnableItem BIOCOAL = null;
 
 	@SubscribeEvent
 	public static void register(Register<Item> e) {
 		//Formatter::off
 		e.getRegistry().registerAll(
-        	new Item(new Properties().group(ActuallyAdditions.GROUP)).setRegistryName("black_quartz")
+        	new Item(new Properties().group(ActuallyAdditions.GROUP)).setRegistryName("black_quartz"),
+			new BurnableItem(new ResourceLocation(ActuallyAdditions.MODID, "tiny_coal"), 200),
+			new BurnableItem(new ResourceLocation(ActuallyAdditions.MODID, "tiny_charcoal"), 200),
+			new BurnableItem(new ResourceLocation(ActuallyAdditions.MODID, "biocoal"), 800)
 		);
 		//Formatter::on
 	}
