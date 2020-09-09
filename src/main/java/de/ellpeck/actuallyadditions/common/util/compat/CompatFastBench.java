f44c4de74a7dbdf184df5cb8fda6c4576a2a1699
Michael <michaelhillcox@gmail.com>
2020-09-09 16:57:31 +0100

    Sorted of fixed FastBench compat

--- src/main/java/de/ellpeck/actuallyadditions/common/util/compat/CompatFastBench.java
+++ src/main/java/de/ellpeck/actuallyadditions/common/util/compat/CompatFastBench.java
@@ -1,29 +1,29 @@
 package de.ellpeck.actuallyadditions.common.util.compat;
 
-import net.minecraft.client.gui.Gui;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.container.Container;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import shadows.fastbench.gui.ContainerFastBench;
 import shadows.fastbench.gui.GuiFastBench;
 
 public class CompatFastBench {
 
-    public static Container getFastBenchContainer(EntityPlayer p, World world) {
-        return new ContainerFastBench(p, world, BlockPos.ORIGIN) {
+    public static Container getFastBenchContainer(PlayerEntity p, World world) {
+        return new ContainerFastBench(0, p, world, BlockPos.ZERO) {
             @Override
-            public boolean canInteractWith(EntityPlayer playerIn) {
+            public boolean canInteractWith(PlayerEntity playerIn) {
                 return true;
             }
         };
     }
 
-    @SideOnly(Side.CLIENT)
-    public static Gui getFastBenchGui(EntityPlayer p, World world) {
-        return new GuiFastBench(p.inventory, world, BlockPos.ORIGIN);
+    @OnlyIn(Dist.CLIENT)
+    public static Screen getFastBenchGui(PlayerEntity p, World world) {
+        return new GuiFastBench(p.inventory, world, BlockPos.ZERO);
     }
 
 }
