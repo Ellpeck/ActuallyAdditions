326ddd770c5b4fc06bd668653dad7bbb9ad13e42
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2021-11-24 12:26:45 -0600

    random jei plugin stuff...

--- src/main/java/de/ellpeck/actuallyadditions/mod/jei/JEIActuallyAdditionsPlugin.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/jei/JEIActuallyAdditionsPlugin.java
@@ -1,52 +1,64 @@
-///*
-// * This file ("JEIActuallyAdditionsPlugin.java") is part of the Actually Additions mod for Minecraft.
-// * It is created and owned by Ellpeck and distributed
-// * under the Actually Additions License to be found at
-// * http://ellpeck.de/actaddlicense
-// * View the source code at https://github.com/Ellpeck/ActuallyAdditions
-// *
-// * © 2015-2017 Ellpeck
-// */
-//
-//package de.ellpeck.actuallyadditions.mod.jei;
-//
-//import de.ellpeck.actuallyadditions.api.ActuallyAdditionsAPI;
-//import de.ellpeck.actuallyadditions.api.booklet.IBookletPage;
-//import de.ellpeck.actuallyadditions.api.recipe.CoffeeIngredient;
-//import de.ellpeck.actuallyadditions.api.recipe.CrusherRecipe;
-//import de.ellpeck.actuallyadditions.api.recipe.EmpowererRecipe;
-//import de.ellpeck.actuallyadditions.api.recipe.LensConversionRecipe;
-//import de.ellpeck.actuallyadditions.mod.blocks.ActuallyBlocks;
-//import de.ellpeck.actuallyadditions.mod.inventory.gui.GuiCoffeeMachine;
-//import de.ellpeck.actuallyadditions.mod.inventory.gui.GuiFurnaceDouble;
-//import de.ellpeck.actuallyadditions.mod.inventory.gui.GuiGrinder;
-//import de.ellpeck.actuallyadditions.mod.items.ActuallyItems;
-//import de.ellpeck.actuallyadditions.mod.jei.booklet.BookletRecipeCategory;
-//import de.ellpeck.actuallyadditions.mod.jei.booklet.BookletRecipeWrapper;
-//import de.ellpeck.actuallyadditions.mod.jei.coffee.CoffeeMachineRecipeCategory;
-//import de.ellpeck.actuallyadditions.mod.jei.coffee.CoffeeMachineRecipeWrapper;
-//import de.ellpeck.actuallyadditions.mod.jei.crusher.CrusherRecipeCategory;
-//import de.ellpeck.actuallyadditions.mod.jei.crusher.CrusherRecipeWrapper;
-//import de.ellpeck.actuallyadditions.mod.jei.empowerer.EmpowererRecipeCategory;
-//import de.ellpeck.actuallyadditions.mod.jei.empowerer.EmpowererRecipeWrapper;
-//import de.ellpeck.actuallyadditions.mod.jei.reconstructor.ReconstructorRecipeCategory;
-//import de.ellpeck.actuallyadditions.mod.jei.reconstructor.ReconstructorRecipeWrapper;
-//import de.ellpeck.actuallyadditions.mod.util.Util;
-//import mezz.jei.api.IModPlugin;
-//import mezz.jei.api.constants.VanillaRecipeCategoryUid;
-//import mezz.jei.api.helpers.IJeiHelpers;
-//import mezz.jei.api.registration.IRecipeCategoryRegistration;
-//import net.minecraft.item.ItemStack;
-//
-//@JEIPlugin
-//public class JEIActuallyAdditionsPlugin implements IModPlugin {
-//
-//    @Override
-//    public void registerCategories(IRecipeCategoryRegistration registry) {
-//        IJeiHelpers helpers = registry.getJeiHelpers();
-//        registry.addRecipeCategories(new CoffeeMachineRecipeCategory(helpers.getGuiHelper()), new CompostRecipeCategory(helpers.getGuiHelper()), new CrusherRecipeCategory(helpers.getGuiHelper()), new ReconstructorRecipeCategory(helpers.getGuiHelper()), new EmpowererRecipeCategory(helpers.getGuiHelper()), new BookletRecipeCategory(helpers.getGuiHelper()));
-//    }
-//
+/*
+ * This file ("JEIActuallyAdditionsPlugin.java") is part of the Actually Additions mod for Minecraft.
+ * It is created and owned by Ellpeck and distributed
+ * under the Actually Additions License to be found at
+ * http://ellpeck.de/actaddlicense
+ * View the source code at https://github.com/Ellpeck/ActuallyAdditions
+ *
+ * © 2015-2017 Ellpeck
+ */
+
+package de.ellpeck.actuallyadditions.mod.jei;
+
+import de.ellpeck.actuallyadditions.mod.ActuallyAdditions;
+import de.ellpeck.actuallyadditions.mod.items.ActuallyItems;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.helpers.IJeiHelpers;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+@JeiPlugin
+public class JEIActuallyAdditionsPlugin implements IModPlugin {
+    public static final ResourceLocation ID = new ResourceLocation(ActuallyAdditions.MODID, "jei_plugin");
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ID;
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registry) {
+        IJeiHelpers helpers = registry.getJeiHelpers();
+        //registry.addRecipeCategories(new CoffeeMachineRecipeCategory(helpers.getGuiHelper()), new CompostRecipeCategory(helpers.getGuiHelper()), new CrusherRecipeCategory(helpers.getGuiHelper()), new ReconstructorRecipeCategory(helpers.getGuiHelper()), new EmpowererRecipeCategory(helpers.getGuiHelper()), new BookletRecipeCategory(helpers.getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registry) {
+        registry.addRecipeCatalyst(new ItemStack(ActuallyItems.CRAFTER_ON_A_STICK.get()), VanillaRecipeCategoryUid.CRAFTING);
+//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockFurnaceDouble.get()), VanillaRecipeCategoryUid.SMELTING);
+//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockGrinder.get()), CrusherRecipeCategory.NAME);
+//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockGrinderDouble.get()), CrusherRecipeCategory.NAME);
+//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockCoffeeMachine.get()), CoffeeMachineRecipeCategory.NAME);
+//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockAtomicReconstructor.get()), ReconstructorRecipeCategory.NAME);
+//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockEmpowerer.get()), EmpowererRecipeCategory.NAME);
+//        registry.addRecipeCatalyst(new ItemStack(ActuallyItems.itemBooklet.get()), BookletRecipeCategory.NAME);
+        //        registry.addRecipeCatalyst(new ItemStack(InitBlocks.blockCompost.get()), CompostRecipeCategory.NAME);
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registry) {
+        //registry.addRecipes(ActuallyAdditionsAPI.BOOKLET_PAGES_WITH_ITEM_OR_FLUID_DATA, BookletRecipeCategory.NAME);
+        //registry.addRecipes(ActuallyAdditionsAPI.COFFEE_MACHINE_INGREDIENTS, CoffeeMachineRecipeCategory.NAME);
+        //registry.addRecipes(ActuallyAdditionsAPI.CRUSHER_RECIPES, CrusherRecipeCategory.NAME);
+        //registry.addRecipes(ActuallyAdditionsAPI.RECONSTRUCTOR_LENS_CONVERSION_RECIPES, ReconstructorRecipeCategory.NAME);
+        //registry.addRecipes(ActuallyAdditionsAPI.EMPOWERER_RECIPES, EmpowererRecipeCategory.NAME);
+        //registry.addRecipes(ActuallyAdditionsAPI.COMPOST_RECIPES, CompostRecipeCategory.NAME);
+    }
+
 //    @Override
 //    public void register(IModRegistry registry) {
 //        IJeiHelpers helpers = registry.getJeiHelpers();
@@ -58,12 +70,7 @@
 //        registry.handleRecipes(EmpowererRecipe.class, EmpowererRecipeWrapper::new, EmpowererRecipeCategory.NAME);
 //        registry.handleRecipes(CompostRecipe.class, CompostRecipeWrapper::new, CompostRecipeCategory.NAME);
 //
-//        registry.addRecipes(ActuallyAdditionsAPI.BOOKLET_PAGES_WITH_ITEM_OR_FLUID_DATA, BookletRecipeCategory.NAME);
-//        registry.addRecipes(ActuallyAdditionsAPI.COFFEE_MACHINE_INGREDIENTS, CoffeeMachineRecipeCategory.NAME);
-//        registry.addRecipes(ActuallyAdditionsAPI.CRUSHER_RECIPES, CrusherRecipeCategory.NAME);
-//        registry.addRecipes(ActuallyAdditionsAPI.RECONSTRUCTOR_LENS_CONVERSION_RECIPES, ReconstructorRecipeCategory.NAME);
-//        registry.addRecipes(ActuallyAdditionsAPI.EMPOWERER_RECIPES, EmpowererRecipeCategory.NAME);
-//        registry.addRecipes(ActuallyAdditionsAPI.COMPOST_RECIPES, CompostRecipeCategory.NAME);
+//
 //
 //        registry.addRecipeClickArea(GuiCoffeeMachine.class, 53, 42, 22, 16, CoffeeMachineRecipeCategory.NAME);
 //        registry.addRecipeClickArea(GuiGrinder.class, 80, 40, 24, 22, CrusherRecipeCategory.NAME);
@@ -79,14 +86,5 @@
 //        blacklist.addIngredientToBlacklist(new ItemStack(ActuallyBlocks.blockColoredLampOn.get(), 1, Util.WILDCARD));
 //        //        blacklist.addIngredientToBlacklist(new ItemStack(InitItems.itemMisc.get(), 1, TheMiscItems.YOUTUBE_ICON.ordinal()));
 //
-//        registry.addRecipeCatalyst(new ItemStack(ActuallyItems.itemCrafterOnAStick.get()), VanillaRecipeCategoryUid.CRAFTING);
-//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockFurnaceDouble.get()), VanillaRecipeCategoryUid.SMELTING);
-//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockGrinder.get()), CrusherRecipeCategory.NAME);
-//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockGrinderDouble.get()), CrusherRecipeCategory.NAME);
-//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockCoffeeMachine.get()), CoffeeMachineRecipeCategory.NAME);
-//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockAtomicReconstructor.get()), ReconstructorRecipeCategory.NAME);
-//        registry.addRecipeCatalyst(new ItemStack(ActuallyBlocks.blockEmpowerer.get()), EmpowererRecipeCategory.NAME);
-//        registry.addRecipeCatalyst(new ItemStack(ActuallyItems.itemBooklet.get()), BookletRecipeCategory.NAME);
-//        //        registry.addRecipeCatalyst(new ItemStack(InitBlocks.blockCompost.get()), CompostRecipeCategory.NAME);
 //    }
-//}
+}
