ed8ea0e616d8c9297e949f9ca76abb40fa7dd11d
Mrbysco <Mrbysco@users.noreply.github.com>
2024-03-09 00:03:51 +0100

    Override stack sensitive isCorrectToolForDrops so it's aware of the current power

--- src/main/java/de/ellpeck/actuallyadditions/mod/items/DrillItem.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/items/DrillItem.java
@@ -278,6 +278,11 @@ public class DrillItem extends ItemEnergy {
                 : HARVEST_LEVEL >= 2));
     }*/
 
+    @Override
+    public boolean isCorrectToolForDrops(ItemStack stack, BlockState state) {
+        return this.getEnergyStored(stack) >= this.getEnergyUsePerBlock(stack) && super.isCorrectToolForDrops(stack, state);
+    }
+
 //    @Override
 //    public int getHarvestLevel(ItemStack stack, ToolType p_getHarvestLevel_2_, @Nullable Player p_getHarvestLevel_3_, @Nullable BlockState p_getHarvestLevel_4_) {
 //        return HARVEST_LEVEL;
