d8ad92c447f5c5a35ef195db8a541bf3923e8401
Mrbysco <Mrbysco@users.noreply.github.com>
2024-10-17 18:39:43 +0200

    Fix Empowered Emeradic Crystal requiring tall grass (Fixes #1410)

--- src/main/java/de/ellpeck/actuallyadditions/data/EmpoweringRecipeGenerator.java
+++ src/main/java/de/ellpeck/actuallyadditions/data/EmpoweringRecipeGenerator.java
@@ -95,12 +95,12 @@ public class EmpoweringRecipeGenerator extends RecipeProvider {
 
         EmpoweringBuilder.builder(ActuallyItems.EMPOWERED_EMERADIC_CRYSTAL.get(), ActuallyItems.EMERADIC_CRYSTAL.get(), 5000, 50, Crystals.EMERALD.conversionColorParticles)
                 .addModifier(Tags.Items.DYES_LIME)
-                .addModifier(Items.TALL_GRASS)
+                .addModifier(Items.SHORT_GRASS)
                 .addModifier(ItemTags.SAPLINGS)
                 .addModifier(Tags.Items.SLIMEBALLS).save(recipeOutput, "emeradic");
         EmpoweringBuilder.builder(ActuallyBlocks.EMPOWERED_EMERADIC_CRYSTAL.get(), ActuallyBlocks.EMERADIC_CRYSTAL.get(), 50000, 500, Crystals.EMERALD.conversionColorParticles)
                 .addModifier(Tags.Items.DYES_LIME)
-                .addModifier(Items.TALL_GRASS)
+                .addModifier(Items.SHORT_GRASS)
                 .addModifier(ItemTags.SAPLINGS)
                 .addModifier(Tags.Items.SLIMEBALLS).save(recipeOutput, "emeradic_block");
 
