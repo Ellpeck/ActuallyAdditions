645dbb2dea6f11e205874c0cef7f31d688f6ca34
Mrbysco <Mrbysco@users.noreply.github.com>
2024-10-31 20:17:13 +0100

    Move the cloche recipes behind a mod loaded condition

--- src/main/java/de/ellpeck/actuallyadditions/data/compat/ClocheRecipes.java
+++ src/main/java/de/ellpeck/actuallyadditions/data/compat/ClocheRecipes.java
@@ -9,9 +9,11 @@ import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
+import net.neoforged.neoforge.common.conditions.ModLoadedCondition;
 
 public class ClocheRecipes {
     public static void buildRecipes(RecipeOutput output) {
+        RecipeOutput loadedOutput = output.withConditions(new ModLoadedCondition("immersiveengineering"));
         ClocheRecipeBuilder.builder()
                 .output(ActuallyItems.CANOLA, 2)
                 .output(ActuallyItems.CANOLA_SEEDS)
@@ -19,7 +21,7 @@ public class ClocheRecipes {
                 .soil(ItemTags.DIRT)
                 .setTime(800)
                 .setRender(new ClocheRenderFunctions.RenderFunctionCrop(ActuallyBlocks.CANOLA.get()))
-                .build(output, ResourceLocation.fromNamespaceAndPath(ActuallyAdditions.MODID, "compat/cloche_canola"));
+                .build(loadedOutput, ResourceLocation.fromNamespaceAndPath(ActuallyAdditions.MODID, "compat/cloche_canola"));
 
         ClocheRecipeBuilder.builder()
                 .output(Items.STRING, 2)
@@ -28,7 +30,7 @@ public class ClocheRecipes {
                 .soil(ItemTags.DIRT)
                 .setTime(800)
                 .setRender(new ClocheRenderFunctions.RenderFunctionCrop(ActuallyBlocks.FLAX.get()))
-                .build(output, ResourceLocation.fromNamespaceAndPath(ActuallyAdditions.MODID, "compat/cloche_flax"));
+                .build(loadedOutput, ResourceLocation.fromNamespaceAndPath(ActuallyAdditions.MODID, "compat/cloche_flax"));
 
         ClocheRecipeBuilder.builder()
                 .output(ActuallyItems.COFFEE_BEANS, 2)
@@ -37,7 +39,7 @@ public class ClocheRecipes {
                 .soil(ItemTags.DIRT)
                 .setTime(800)
                 .setRender(new ClocheRenderFunctions.RenderFunctionCrop(ActuallyBlocks.COFFEE.get()))
-                .build(output, ResourceLocation.fromNamespaceAndPath(ActuallyAdditions.MODID, "compat/cloche_coffee"));
+                .build(loadedOutput, ResourceLocation.fromNamespaceAndPath(ActuallyAdditions.MODID, "compat/cloche_coffee"));
 
         ClocheRecipeBuilder.builder()
                 .output(ActuallyItems.RICE, 2)
@@ -46,6 +48,6 @@ public class ClocheRecipes {
                 .soil(ItemTags.DIRT)
                 .setTime(800)
                 .setRender(new ClocheRenderFunctions.RenderFunctionCrop(ActuallyBlocks.RICE.get()))
-                .build(output, ResourceLocation.fromNamespaceAndPath(ActuallyAdditions.MODID, "compat/cloche_rice"));
+                .build(loadedOutput, ResourceLocation.fromNamespaceAndPath(ActuallyAdditions.MODID, "compat/cloche_rice"));
     }
 }
